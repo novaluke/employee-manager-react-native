d79e520428bff2e88bfad5e50c2162be284c9135
Luke Mayhew <luke@webuildlegends.com>
2018-08-30 09:46:22 -0600

    Set up firebase

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import firebase from "firebase";
 
 import reducers from "./reducers";
+import firebaseJson from "../firebaseConfig.json";
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp(firebaseJson);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
