c2d1d47bb4359a16eaa9be421fa7d94a231754ce
Luke Mayhew <luke@webuildlegends.com>
2018-08-30 11:54:34 -0600

    Add tests for App component

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,30 @@
+import "jest-enzyme";
+import React from "react";
+
+import { shallow } from "enzyme";
+import firebase from "firebase";
+import { create } from "react-test-renderer";
+
+import firebaseConfigJson from "../firebaseConfig.json";
+import App from "../src/App";
+
+import { store } from "../src/store";
+
+jest.mock("../src/components/InitializingScreen", () => "InitializingScreen");
+
+describe("root App component", () => {
+  it("provides the store to all components", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toHaveTagName("Provider");
+    expect(wrapper).toHaveProp("store", store);
+  });
+
+  it("initializes the firebase instance", () => {
+    expect(firebase.initializeApp).toHaveBeenCalledWith(firebaseConfigJson);
+  });
+
+  it("loads InitializingScreen as the initial screen", () => {
+    const { root } = create(<App />);
+    expect(root.findAllByType("InitializingScreen").length).toBe(1);
+  });
+});
