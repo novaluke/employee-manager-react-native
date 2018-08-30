88ed661448f8314ea623e4472d5e6d6444aaa1b8
Luke Mayhew <luke@webuildlegends.com>
2018-08-30 12:54:37 -0600

    Add tests for CardSection

---
+++ __tests__/components/common/CardSection.tsx
@@ -0,0 +1,35 @@
+import "jest-enzyme";
+import React from "react";
+
+import { shallow } from "enzyme";
+import reactTestRenderer from "react-test-renderer";
+
+import { StyleSheet } from "react-native";
+
+import { CardSection } from "../../../src/components/common";
+
+describe("CardSection", () => {
+  it("renders correctly", () => {
+    const props = { children: ["Child component"] };
+    const tree = reactTestRenderer.create(<CardSection {...props} />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("adopts styles passed to it", () => {
+    const styles = StyleSheet.create({
+      testStyle: {
+        backgroundColor: "papayawhip",
+      },
+    });
+    const appliedStyles = shallow(
+      <CardSection style={styles.testStyle} />,
+    ).prop("style");
+    expect(appliedStyles[appliedStyles.length - 1]).toEqual(styles.testStyle);
+  });
+
+  it("passes props through to the underlying view", () => {
+    const props = { foo: "bar" };
+    const wrapper = shallow(<CardSection {...props} />);
+    expect(wrapper.prop("foo")).toBe(props.foo);
+  });
+});
